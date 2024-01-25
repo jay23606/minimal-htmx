2d40e26c6fbd3ea9ca82c154c8b190654625b18b
Jay <jay_23606@yahoo.com>
2024-01-25 09:02:04 -0500

    Update hxClass.js

--- hxClass.js
+++ hxClass.js
@@ -1,17 +1,10 @@
 (() => {
-	// Dynamically load js-yaml library
 	const script = document.createElement('script');
 	script.src = 'https://cdn.jsdelivr.net/npm/js-yaml/dist/js-yaml.min.js';
-	script.onload = () => {
-	  // After js-yaml is loaded, execute the main script
-	  loadAndApplyAttributes();
-	};
+	script.onload = () => loadAndApplyAttributes();
 	document.head.appendChild(script);
-
-	// Main script
 	const loadAndApplyAttributes = async () => {
-	  const applyAttributes = (element, attributes) => Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
-
+	  const applyAttributes = (element, attributes) => Object.entries(attributes).forEach(([key, value]) => element.hasAttribute(key) || element.setAttribute(key, value));
 	  const applyClassAttributes = async (element, classNames) => {
 		const yamlData = document.getElementById('classAttributeMap').textContent;
 		const classAttributeMap = jsyaml.load(yamlData);
@@ -21,8 +14,6 @@
 		  applyAttributes(element, classAttributes);
 		});
 	  };
-
-	  // Iterate over elements with hx-class attribute
 	  document.querySelectorAll('[hx-class]').forEach(element => {
 		const classNames = element.getAttribute('hx-class');
 		applyClassAttributes(element, classNames);
